918d53b1daece4ccbd8c0b71940d292310f804c4
Qiming Ye <yqm_leaf@163.com>
2020-10-08 12:51:47 +0800

    Change sidebar to bootstrap style.

--- web/shami/app.py
+++ web/shami/app.py
@@ -2,8 +2,9 @@
 
 import os
 
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
+import werkzeug.exceptions as ex
 
 import click
 
@@ -24,6 +25,10 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
+class PathDoesNotExist(ex.HTTPException):
+    code = 404
+    description = "<p>Path does not exist.</p>"
+
 class ShamiLog(db.Model):
     __tablename__ = "shamilog"
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +36,10 @@ class ShamiLog(db.Model):
     feature = db.Column(db.String(20))
     version = db.Column(db.String(20))
     logurl = db.Column(db.String(50))
+
+@app.errorhandler(404)
+def no_such_path(e):
+    return "Path does not exist."
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop')
@@ -46,25 +55,26 @@ def index():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
-        flash(f"Welcome home, {username}")
+        flash(f"Welcome, {username}")
     return render_template('login.html', form=form)
 
 @app.route('/logs')
 def logs():
     shamilogs = ShamiLog.query.all()
-    click.echo(shamilogs)
     return render_template('logs.html', shamilogs=shamilogs)
 
 @app.route('/tasks')
 def tasks():
     """List tasks"""
     tasks_path = os.path.join(app.config['SHAMI_ROOT_DIR'], "tasks")
+    if not os.path.isdir(tasks_path):
+        abort(404)
     tasks_files = []
     for t in list_file(tasks_path):
         tasks_files.append(os.path.basename(t))
     return render_template('tasks.html', tasks_files=tasks_files)
 
-@app.route('/submit_task', methods=['GET', 'POST'])
+@app.route('/submit-task', methods=['GET', 'POST'])
 def submit_task():
     return render_template('submit_task.html')
 
@@ -73,13 +83,32 @@ def register():
     """Register for an account."""
     return render_template('register.html')
 
-@app.route('/task_overview', methods=['GET', 'POST'])
+@app.route('/task-overview', methods=['GET', 'POST'])
 def task_overview():
     return 'This is the task overview page.'
+
+@app.route('/resources')
+def resources():
+    """The resource page"""
+    resouces_path = os.path.join(app.config['SHAMI_ROOT_DIR'], 'resources')
+    if not os.path.isdir(resouces_path):
+        abort(404)
+    res = []
+    for r in list_file(resouces_path):
+        _r = {
+                "abspath": r,
+                "path": os.path.basename(r).strip('.lck'),
+                # "pc1.lck" means resource is locked by the system.
+                "locked": True if r.endswith('.lck') else False
+             }
+
+        res.append(_r)
+    return render_template('resources.html', res=res)
 
 @app.cli.command()
 @click.option('--count', default=20, help='Quantity of fake data')
 def create_fake_data(count):
+    """Create fake data"""
     from faker import Faker
     
     fake = Faker()
